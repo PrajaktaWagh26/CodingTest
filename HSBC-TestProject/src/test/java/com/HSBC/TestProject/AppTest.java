0a36f74ac6c193eb88e9b70a8491590783871d38
PrajaktaWagh26 <wagh.prajakta@outlook.com>
2020-04-22 20:20:52 +0530

    Created maven project

---
+++ HSBC-TestProject/src/test/java/com/HSBC/TestProject/AppTest.java
@@ -0,0 +1,20 @@
+package com.HSBC.TestProject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
